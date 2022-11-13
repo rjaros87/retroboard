736ecea7881ebe029d21573ae2788c4afdbf6560
Radoslaw Jaros <jaros_radek@op.pl>
2022-11-13 20:09:54 +0100

    Fix unit test

--- src/test/java/io/github/rjaros87/RetroboardTest.java
+++ src/test/java/io/github/rjaros87/RetroboardTest.java
@@ -1,21 +1,55 @@
 package io.github.rjaros87;
 
-import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.PropertySource;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import jakarta.inject.Inject;
+import java.util.Map;
 
-@MicronautTest
 class RetroboardTest {
 
-    @Inject
-    EmbeddedApplication<?> application;
+    private static final String REDIS_DOCKER_NAME = "bitnami/redis:latest";
+    private static final Integer REDIS_PORT = 6379;
+    private static final String REDIS_PWD = "str0ng_passw0rd";
+
+    private static final GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_NAME))
+            .withExposedPorts(6379)
+            .withEnv(Map.of("REDIS_PASSWORD", REDIS_PWD));
+
+    private static ApplicationContext context;
+
+    @BeforeAll
+    public static void initUnitTest() {
+        redis.start();
+
+        context = ApplicationContext.run(PropertySource.of(
+            "test", Map.of(
+                    "redis.servers.default.host", redis.getHost(),
+                    "redis.servers.default.port", redis.getMappedPort(REDIS_PORT),
+                    "redis.servers.default.password", REDIS_PWD,
+                    "redis.servers.pub.host", redis.getHost(),
+                    "redis.servers.pub.port", redis.getMappedPort(REDIS_PORT),
+                    "redis.servers.pub.password", REDIS_PWD,
+                    "redis.servers.sub.host", redis.getHost(),
+                    "redis.servers.sub.port", redis.getMappedPort(REDIS_PORT),
+                    "redis.servers.sub.password", REDIS_PWD
+            ))
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        redis.stop();
+    }
 
     @Test
     void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+        Assertions.assertTrue(context.isRunning());
     }
 
 }
